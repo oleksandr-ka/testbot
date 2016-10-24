dca8d90347216644998a0844740d76e1ce73c58b
Oleksandr Kabarivskiy <oleksandr.kabarivskiy@gmail.com>
2016-10-24 15:41:08 +0300

    changes

--- config/initializers/messenger_platform.rb
+++ config/initializers/messenger_platform.rb
@@ -1,5 +1,5 @@
 MessengerPlatform.configure do |config|
   # config.page_id = 'testbot' # => messenger token
   # config.token = 'EAAThNY2jwBUBAAozpvVXoPwx0ZCFtTO0ZBRfmoMXNbbctl7R8MUEOZAEgDG4KPtwgdIuZAmpSykcZB88WO6B0wEjWUiZBfoNUPui9ggCm6RTqvWJmYbVYyjqBc3qG3edAB7hx0WllGXrG5sw2XZBHdlyjKGl2x17Px7Chgtk0uNNwZDZD' # => Page ID
-  config.token = 'EAAQIgUlE0UMBAK21AV3rmAquIeWTTF63pPKHkHJjiz32cymR9ZCvZCAdtxBiN64QyyfQEy4M7iG027f5ibZCKqFL1u2ErjKL6JtqVPpdIY9NXD7TVaBL3yErypXpV8xN5YSwBCmm9AV4VOUh6yacx7L0OIRiIdKUNpMAIk7okdESowBohq7' # => Page ID
+  config.token = 'EAAHetZA4szIwBAKe8F2Vf7ZCXpwAWdZBUpFatdRptZAk8NZABZAQrjeLXGU3uPVsHFEAUYHZC6ZAdDUyAe1n1rfMgntildFB3iOCZBkbHaEouJSHyUo1yGejtCCJ9Og9GnVsUqVNbItjOvFHUqkA1Kzr8nKPispqZA2HywQoD0wKFBdzmVgCCyxZCBk' # => Page ID
 end
