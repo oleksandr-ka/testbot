02461cf5141acb3cd7897bf86bbbf07769b60e8f
Oleksandr Kabarivskiy <oleksandr.kabarivskiy@gmail.com>
2016-12-29 16:33:41 +0200

    changes

--- lib/wiki.rb
+++ lib/wiki.rb
@@ -5,7 +5,22 @@ class Wiki
 
     def search(text)
       Wikipedia.Configure {domain "#{I18n.locale}.wikipedia.org"}
-      return Wikipedia.find(text)
+      return Wikipedia.find(prepare_query(text))
+    end
+
+    # private
+
+    def prepare_query(text)
+      query = []
+      %w(capitalize upcase downcase).each do |m|
+        query << text.split(/[\s,-]/).map(&:mb_chars).map(&m.to_sym).map(&:to_s)
+      end
+      prepared_query = []
+      query.each do |texts|
+        prepared_query << texts.join(' ')
+        prepared_query << texts.join('-')
+      end
+      return prepared_query.join('|')
     end
   end
 end
