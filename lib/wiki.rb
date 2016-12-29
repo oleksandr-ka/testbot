7bc8b2eea93a389ef62fe796fa78e02aa3d21ffc
Oleksandr Kabarivskiy <oleksandr.kabarivskiy@gmail.com>
2016-12-29 17:20:29 +0200

    changes

--- lib/wiki.rb
+++ lib/wiki.rb
@@ -12,7 +12,7 @@ class Wiki
 
     def prepare_query(text)
       query = []
-      %w(capitalize upcase downcase).each do |m|
+      %w(capitalize downcase upcase).each do |m|
         query << text.split(/[\s,-]/).map(&:mb_chars).map(&m.to_sym).map(&:to_s)
       end
       prepared_query = []
